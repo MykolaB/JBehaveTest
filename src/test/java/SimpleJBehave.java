42ab616013b104e7252b6ef78a104a573f6d4813
MykolaB <!q1w2e3r4>
2015-10-08 16:39:54 +0300

    Initial commit

---
+++ src/test/java/SimpleJBehave.java
@@ -0,0 +1,19 @@
+/**
+ * Created by NBiletskiy on 10/8/2015.
+ */
+import java.util.Arrays;
+import java.util.List;
+
+import org.jbehave.core.embedder.Embedder;
+
+public class SimpleJBehave {
+
+    private static Embedder embedder = new Embedder();
+    private static List<String> storyPaths = Arrays
+            .asList("Math.story");
+
+    public static void main(String[] args) {
+        embedder.candidateSteps().add(new ExampleSteps());
+        embedder.runStoriesAsPaths(storyPaths);
+    }
+}
